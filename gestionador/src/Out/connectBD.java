98648b91281ebb5e3a7c69ccaed8d0756862bb95
miguel <miguel@mike.com>
2016-11-02 15:15:55 -0700

    Diseño de la interfaz
    solo el diseño del ingreso a la BD y recuperacion de los datos de
    detalle de venta

---
+++ gestionador/src/Out/connectBD.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Out;
+ 
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Charlie
+ */
+public class connectBD {
+    Connection conect = null;
+    public Connection conexion()
+        {
+            try {
+                //Cargamos el Driver MySQL
+                Class.forName("com.mysql.jdbc.Driver");
+                //direccion de la base de datos
+                conect = DriverManager.getConnection("jdbc:mysql://localhost/bd","root","");
+                System.out.println("conexion establecida");
+                JOptionPane.showMessageDialog(null,"Conectado");
+                
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("error al conexion");
+                JOptionPane.showMessageDialog(null,"Error de conexion"+e);
+            }
+            return conect;
+        }
+     
+}
+
